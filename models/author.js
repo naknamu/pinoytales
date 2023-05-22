09c6ca7a47ad2372f54c87b54eee91dfe9aeed7c
naknamu <jodeldelvalle@gmail.com>
2023-05-22 17:29:56 +0800

    Edited schema

--- models/author.js
+++ models/author.js
@@ -2,33 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-    first_name: {
-        type: String
-    },
-    family_name: {
-        type: String
-    },
-    date_of_birth: {
-        type: Date
-    },
-    date_of_death: {
-        type: Date
+    name: {
+        type: String,
+        required: true
     }
 });
 
-// Virtual for author's full name
-AuthorSchema.virtual("name").get(function() {
-    let fullname = "";
-    if (this.first_name && this.family_name) {
-        fullname = `${this.family_name, this.first_name}`;
-    }
-
-    return fullname;
-})
 
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
-    return `/author/${this.first_name-this.family_name}`;
+    return `/author/${this.name}`;
 });
 
 // Export model
