5bead827e839ad57544f759d49a26e77f2188578
naknamu <jodeldelvalle@gmail.com>
2023-05-22 13:59:28 +0800

    Added controller and route for author

---
+++ controllers/authorController.js
@@ -0,0 +1,53 @@
+const Author = require('../models/author');
+const asyncHandler = require('express-async-handler')
+
+// Display list of all authors
+author_list = asyncHandler( async(req, res, next) => {
+    res.send("NOT IMPLEMENTED: author LIST")
+})
+
+// Display detail page for a specific author.
+author_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: author detail: ${req.params.authorname}`);
+});
+
+// Display author create form on GET.
+author_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: author create GET");
+});
+
+// Handle author create form on POST.
+author_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: author create POST");
+});
+
+// Display author delete form on GET.
+author_delete_get = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: author delete GET: ${req.params.authorname} `);
+});
+
+// Handle author delete on POST.
+author_delete_post = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: author delete POST: ${req.params.authorname}`);
+});
+
+// Display author update form on GET.
+author_update_get = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: author update GET: ${req.params.authorname}`);
+});
+
+// Handle author update on POST.
+author_update_post = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: author update POST: ${req.params.authorname}`);
+});
+
+module.exports = {
+    author_list,
+    author_detail,
+    author_create_get,
+    author_create_post,
+    author_delete_get,
+    author_delete_post,
+    author_update_get,
+    author_update_post
+}
--- routes/library.js
+++ routes/library.js
@@ -26,9 +26,19 @@ const {
     genre_update_post
 } = require('../controllers/genreController');
 
+const {
+    author_list,
+    author_detail,
+    author_create_get,
+    author_create_post,
+    author_delete_get,
+    author_delete_post,
+    author_update_get,
+    author_update_post
+} = require('../controllers/authorController');
 
 
-// Get API homepage
+// Get HOMEPAGE
 router.get("/", home);
 
 /// TALE ROUTES ///
@@ -83,6 +93,33 @@ router.get("/genres", genre_list);
 
 // GET request for a single genre
 router.get("/genres/:genrename", genre_detail);
+
+
+/// AUTHOR ROUTES ///
+
+// GET request for creating author
+router.get("/author/create", author_create_get);
+
+// POST request for creating author
+router.post("/author/create", author_create_post);
+
+// GET request for deleting author
+router.get("/author/:authorname/delete", author_delete_get);
+
+// POST request for deleting author
+router.post("/author/:authorname/delete", author_delete_post);
+
+// GET request for updating author
+router.get("/author/:authorname/update", author_update_get);
+
+// POST request for updating author
+router.post("/author/:authorname/update", author_update_post);
+
+// GET request for list of authors
+router.get("/authors", author_list);
+
+// GET request for a single author
+router.get("/authors/:authorname", author_detail);
 
 
 
