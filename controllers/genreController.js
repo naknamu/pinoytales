c8af94b27dd3b33b5594b34c5477d54b561bfaec
naknamu <jodeldelvalle@gmail.com>
2023-05-22 13:33:32 +0800

    Added genre controller

---
+++ controllers/genreController.js
@@ -0,0 +1,53 @@
+const Genre = require('../models/genre');
+const asyncHandler = require('express-async-handler')
+
+// Display list of all Categories
+genre_list = asyncHandler( async(req, res, next) => {
+    res.send("NOT IMPLEMENTED: genre LIST")
+})
+
+// Display detail page for a specific genre.
+genre_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: genre detail: ${req.params.genrename}`);
+});
+
+// Display genre create form on GET.
+genre_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: genre create GET");
+});
+
+// Handle genre create form on POST.
+genre_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: genre create POST");
+});
+
+// Display genre delete form on GET.
+genre_delete_get = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: genre delete GET: ${req.params.genrename} `);
+});
+
+// Handle genre delete on POST.
+genre_delete_post = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: genre delete POST: ${req.params.genrename}`);
+});
+
+// Display genre update form on GET.
+genre_update_get = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: genre update GET: ${req.params.genrename}`);
+});
+
+// Handle genre update on POST.
+genre_update_post = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: genre update POST: ${req.params.genrename}`);
+});
+
+module.exports = {
+    genre_list,
+    genre_detail,
+    genre_create_get,
+    genre_create_post,
+    genre_delete_get,
+    genre_delete_post,
+    genre_update_get,
+    genre_update_post
+}
